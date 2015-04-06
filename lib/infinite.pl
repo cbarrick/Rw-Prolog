6528661c182e6a877114d1c1855c21620504f13a
Chris Barrick <cbarrick1@gmail.com>
2015-04-06 00:34:51 -0400

    Update libs

--- lib/infinite.pl
+++ lib/infinite.pl
@@ -6,3 +6,8 @@ b(X) := z(X).
 
 % An infinite counting example
 s(X) := s(Y) :- Y is X + 1.
+
+
+% A different counting example
+c(_+1+1+1) := _ :- !, fail.  % Limit rewrite to max of X+1+1+1
+c(X)       := c(X+1).
--- lib/rw_lists.pl
+++ lib/rw_lists.pl
@@ -7,6 +7,11 @@ nth(N, [_|T]) := nth(N0, T) :-
 	N0 is N - 1.
 
 
-length(List)    := length(0, List).
-length(N,[])    := N :- !.
-length(N,[_|T]) := length(N+1,T).
+length(List)       := rw_length(0, List).
+rw_length(N,[])    := N :- !.
+rw_length(N,[_|T]) := rw_length(N+1,T).
+
+
+reverse(List)     := reverse(List, []).
+reverse([], X)    := X.
+reverse([H|T], X) := reverse(T, [H|X]).
